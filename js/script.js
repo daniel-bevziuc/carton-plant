4bb80a492ace3d964238201ac56e53b1d8d47c08
Daniel Bevziuc <bevziuc.daniel@gmail.com>
2019-02-10 13:29:54 +0000

    final project for carton plant

---
+++ js/script.js
@@ -0,0 +1,45 @@
+// console.log("re");
+
+// var myFirstVar = 50;
+
+// myFirstVar = 25;
+
+// console.log(myFirstVar);
+
+// console.log(4+6);
+// console.log(4+ "lol");
+// console.log("l" + "o" + "l");
+
+// var incr = 10,
+// 		decr = 10;
+
+// incr++;
+// decr--;
+
+// console.log(incr);
+// console.log(decr);
+
+// console.log(11%3);
+
+// var num = "5";
+// console.log(num != 5);
+// console.log(num === 5);
+
+// var number = 5;
+// var string = "Hello world";
+// var symbol;
+// true;
+// false;
+
+// console.log(6/2 == 3);
+// console.log(10/5 == 3);
+
+// var und;
+
+// console.log(und);
+
+// var obj = {};
+
+// console.log(typeof obj);
+// console.log(typeof string);
+
